c992fa2a721aa7dc5e08572e1b4c035936239dd6
FireInTheHol <142295502+FireInTheHol@users.noreply.github.com>
2023-08-29 18:28:51 +0900

    Data_and_Operations
    under Data_and_Operations

---
+++ Casting.java
@@ -0,0 +1,24 @@
+package Data_and_Opertaion;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		 	double a = 1.1;
+		 	System.out.println(a);
+	        double b = 1;
+	        
+	         
+	        System.out.println(b);
+	         
+	       
+	        
+	        int e = (int) 1.1;
+	        System.out.println(e);
+	         
+	        // 1 to String 
+	        String f = Integer.toString(1);
+	        System.out.println(f.getClass());
+
+	}
+
+}
